b83eccb69ae04ea5ea735578b340c8e665a7fd7c
Soni Darshan <soni.darshan0209@gmail.com>
2024-11-26 18:30:51 +0530

    api_url update

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -5,7 +5,9 @@ axios.defaults.withCredentials = true;
 
 
 
-axios.defaults.baseURL = 'http://localhost:8000/api/v1/users';
+// axios.defaults.baseURL = 'http://localhost:8000/api/v1/users';
 
+// Update baseURL to the deployed backend URL
+axios.defaults.baseURL = 'https://cineverse-fnr5.onrender.com/api/v1/users';
 
 export default axios;
